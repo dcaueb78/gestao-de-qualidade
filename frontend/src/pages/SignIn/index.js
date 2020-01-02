beba93a00dc6c850c419cad7326ed0c7dc1253b9
dcaueb78 <caue1017@hotmail.com>
2020-01-02 01:17:54 -0200

    Add unform

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.png';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Qualyteam" />
-      <form>
-        <input type="text" placeholder="Full name" />
-        <input type="email" placeholder="Your best email" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Full name" required />
+        <Input
+          name="email"
+          type="email"
+          placeholder="Your best email"
+          required
+        />
 
         <button type="submit">Login</button>
         <a href="https://qualyteam.com/pb/precos/" target="blank">
           Take a look at our prices
         </a>
-      </form>
+      </Form>
     </>
   );
 }
