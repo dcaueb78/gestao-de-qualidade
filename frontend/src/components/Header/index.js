ac7925047951895f80c4341bd8cc4e728b294cf8
dcaueb78 <caue1017@hotmail.com>
2020-01-02 04:18:52 -0200

    Header Styles

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo-black.png';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="Qualyteam" />
+          <Link to="dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Profile>
+            <div>
+              <strong>Nome do gestor</strong>
+              <span>Minha empresa</span>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Nome do gestor"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
